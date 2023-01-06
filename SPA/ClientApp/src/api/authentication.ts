951a7283647f49a60e766a945d5aea838bbc2fdb
Artemii Kurganov <aokurganov@mail.ru>
2023-01-06 18:45:30 +0300

    return url

--- SPA/ClientApp/src/api/authentication.ts
+++ SPA/ClientApp/src/api/authentication.ts
@@ -1,51 +1,57 @@
-﻿import {V1LoginDto} from "./types";
+﻿import { V1LoginDto } from './types';
 
 const post = async (url: string, content: any) => {
-    return await fetch(url, {
-        mode: 'no-cors',
-        method: "POST",
-        body: JSON.stringify(content),
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-    });
-}
+  return await fetch(url, {
+    mode: 'no-cors',
+    method: 'POST',
+    body: JSON.stringify(content),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    redirect: 'manual',
+  });
+};
 
 const get = async (url: string) => {
-    return await fetch(url, {
-        mode: 'no-cors',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-    });
-}
+  return await fetch(url, {
+    mode: 'no-cors',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+};
 
 export enum AuthenticationStatus {
-    Authenticated = "Authenticated",
-    Unauthenticated = "Unauthenticated",
-    NoInfo = "NoInfo"
+  Authenticated = 'Authenticated',
+  Unauthenticated = 'Unauthenticated',
+  NoInfo = 'NoInfo',
 }
 
 export const login = async (loginDto: V1LoginDto) => {
-    const response = await post("/account/signin", loginDto);
-    if (response.ok) {
-        return AuthenticationStatus.Authenticated;
-    }
-    if (response.status === 401) {
-        return AuthenticationStatus.Unauthenticated;
-    }
-    return AuthenticationStatus.NoInfo;
-}
+  const response = await post('/account/signin', loginDto);
+  if (response.ok) {
+    return response.url;
+  }
+  if (response.status === 401) {
+    return AuthenticationStatus.Unauthenticated;
+  }
+  return AuthenticationStatus.NoInfo;
+};
 
-export const loginViaExternalProvider = async (provider: string, returnUrl: string) => {
-    const response = await get(`/account/signin-external?provider=${provider}&returnUrl=${returnUrl}`);
-    if (response.ok) {
-        return AuthenticationStatus.Authenticated;
-    }
-    if (response.status === 401) {
-        return AuthenticationStatus.Unauthenticated;
-    }
-    return AuthenticationStatus.NoInfo;
-}
+export const loginViaExternalProvider = async (
+  provider: string,
+  returnUrl: string
+) => {
+  const response = await get(
+    `/account/signin-external?provider=${provider}&returnUrl=${returnUrl}`
+  );
+  if (response.ok) {
+    return AuthenticationStatus.Authenticated;
+  }
+  if (response.status === 401) {
+    return AuthenticationStatus.Unauthenticated;
+  }
+  return AuthenticationStatus.NoInfo;
+};
