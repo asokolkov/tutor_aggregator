f6cad35dc4b8c9a07b00838ac9d8dec43d56731f
Aleksey <a.e.sokolkov@gmail.com>
2023-01-21 18:57:47 +0500

    fix: api, better automapper

--- SPA/Data/ApplicationContext.cs
+++ SPA/Data/ApplicationContext.cs
@@ -5,12 +5,16 @@ using Microsoft.EntityFrameworkCore;
 
 internal sealed class ApplicationContext : DbContext
 {
-    public DbSet<TutorEntity> Tutors { get; set; } = null!;
-    public DbSet<LessonEntity> Lessons { get; set; } = null!;
-    public DbSet<StudentEntity> Students { get; set; } = null!;
-    public DbSet<ReviewEntity> Reviews { get; set; } = null!;
-    public DbSet<LocationEntity> Locations { get; set; } = null!;
-    public DbSet<SubjectEntity> Subjects { get; set; } = null!;
+    public DbSet<TutorEntity> Tutors { get; set; }
+    public DbSet<AwardEntity> Awards { get; set; }
+    public DbSet<TutorContactEntity> TutorsContacts { get; set; }
+    public DbSet<StudentContactEntity> StudentsContacts { get; set; }
+    public DbSet<EducationEntity> Educations { get; set; }
+    public DbSet<LessonEntity> Lessons { get; set; }
+    public DbSet<StudentEntity> Students { get; set; }
+    public DbSet<ReviewEntity> Reviews { get; set; }
+    public DbSet<LocationEntity> Locations { get; set; }
+    public DbSet<JobEntity> Jobs { get; set; }
 
     public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
     {
@@ -24,7 +28,9 @@ internal sealed class ApplicationContext : DbContext
         modelBuilder.Entity<TutorEntity>().Navigation(e => e.Awards).AutoInclude();
         modelBuilder.Entity<TutorEntity>().Navigation(e => e.Lessons).AutoInclude();
         modelBuilder.Entity<TutorEntity>().Navigation(e => e.Reviews).AutoInclude();
-
+        modelBuilder.Entity<TutorEntity>().Navigation(e => e.Job).AutoInclude();
+        modelBuilder.Entity<TutorEntity>().Navigation(e => e.Location).AutoInclude();
+        
         modelBuilder.Entity<StudentEntity>().Navigation(e => e.Contacts).AutoInclude();
         modelBuilder.Entity<StudentEntity>().Navigation(e => e.Lessons).AutoInclude();
         modelBuilder.Entity<StudentEntity>().Navigation(e => e.Reviews).AutoInclude();
