14f1bbf49a16fecea05182e0b0b2649c91501ce2
Ilya Zhdanov <zdravulin@gmail.com>
2023-01-24 01:03:47 +0500

    feat: add navigate to search page after registration

--- SPA/Extensions/ServiceCollectionExtensions.cs
+++ SPA/Extensions/ServiceCollectionExtensions.cs
@@ -58,11 +58,12 @@ internal static class ServiceCollectionExtensions
         services.AddScoped<IRequestHandler<GetSubjectsQuery, List<Subject>>, GetSubjectQueryHandler>();
 
         services.AddScoped<IRequestHandler<GetUserQuery, User>, GetUserQueryHandler>();
-        
+
         services.AddScoped<IRequestHandler<GetAvatarQuery, byte[]>, GetAvatarQueryHandler>();
         services.AddScoped<IRequestHandler<CreateAvatarCommand, byte[]>, CreateAvatarCommandHandler>();
 
-        services.AddScoped<IRequestHandler<GetStudentLessonsQuery, ICollection<Lesson>>, GetStudentLessonsQueryHandler>();
+        services
+            .AddScoped<IRequestHandler<GetStudentLessonsQuery, ICollection<Lesson>>, GetStudentLessonsQueryHandler>();
 
         services.AddScoped<IUserService, UserService>();
         services.AddScoped<IUserRepository, UserRepository>();
