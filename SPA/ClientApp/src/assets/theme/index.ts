b9d4ee13ef90d1c428172cbe718ddc8fc6002688
Artemii Kurganov <aokurganov@mail.ru>
2023-04-09 14:34:03 +0500

    Feat: Варианты для текста в провайдере темы

--- SPA/ClientApp/src/assets/theme/index.ts
+++ SPA/ClientApp/src/assets/theme/index.ts
@@ -1,6 +1,11 @@
 import { extendTheme } from '@chakra-ui/react';
 import { colors } from './colors';
-import { fontSizes } from './fontSizes';
+import { textVariants } from './textVariants';
 
-const theme = extendTheme({ colors, fontSizes });
+const theme = extendTheme({
+  colors,
+  components: {
+    ...textVariants,
+  },
+});
 export default theme;
---
+++ SPA/ClientApp/src/assets/theme/textVariants.ts
@@ -0,0 +1,18 @@
+export const textVariants = {
+  Text: {
+    baseStyle: {
+      fontSize: '18px',
+      fontWeight: 'normal',
+    },
+    variants: {
+      semibold: {
+        fontSize: '18px',
+        fontWeight: 'semibold',
+      },
+      'big-semibold': {
+        fontSize: '20px',
+        fontWeight: 'semibold',
+      },
+    },
+  },
+};
