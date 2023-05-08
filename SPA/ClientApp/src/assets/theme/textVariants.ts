771c6646b6547a4e4c960fb1dea0ccd64e586b79
Mikhail Lanets <theoilside@gmail.com>
2023-05-09 04:08:36 +0500

    Внес цветовые и текстовые стили из Фигмы

--- SPA/ClientApp/src/assets/theme/textVariants.ts
+++ SPA/ClientApp/src/assets/theme/textVariants.ts
@@ -1,17 +1,82 @@
 export const textVariants = {
-  Text: {
+  regular: {
     baseStyle: {
+      fontFamily: 'Inter',
       fontSize: '18px',
-      fontWeight: 'normal',
+      fontWeight: 'regular',
+      lineHeight: '130%',
     },
     variants: {
-      semibold: {
-        fontSize: '18px',
-        fontWeight: 'semibold',
+      bold: {
+        fontWeight: 'semi-bold',
+        lineHeight: '150%',
       },
-      'big-semibold': {
+      h1: {
+        fontSize: '32px',
+        fontWeight: 'bold',
+        lineHeight: '120%',
+      },
+      h2: {
+        fontSize: '24px',
+        fontWeight: 'bold',
+        lineHeight: '133%',
+      },
+      h3: {
         fontSize: '20px',
-        fontWeight: 'semibold',
+        fontWeight: 'semi-bold',
+        lineHeight: '130%',
+      },
+    },
+  },
+  misc: {
+    variants: {
+      link: {
+        fontFamily: 'Inter',
+        fontSize: '22px',
+        fontWeight: 'medium',
+        lineHeight: '150%',
+        textDecoration: 'underline',
+      },
+      field_title: {
+        fontFamily: 'Inter',
+        fontSize: '16px',
+        fontWeight: 'regular',
+        lineHeight: '150%',
+      },
+      button: {
+        fontFamily: 'Inter',
+        fontSize: '18px',
+        fontWeight: 'semi-bold',
+        lineHeight: '130%',
+      },
+      logo_addition: {
+        fontFamily: 'Inter',
+        fontSize: '16px',
+        fontWeight: 'regular',
+        lineHeight: '175%',
+        textDecoration: 'underline',
+      },
+      small_button: {
+        fontFamily: 'Inter',
+        fontSize: '14px',
+        fontWeight: 'semi-bold',
+        lineHeight: '150%',
+      },
+    },
+  },
+  brand: {
+    variants: {
+      logo: {
+        fontFamily: 'PT Sans',
+        fontSize: '32px',
+        fontWeight: 'bold',
+        lineHeight: '120%',
+      },
+      h1: {
+        fontFamily: 'PT Sans',
+        fontSize: '40px',
+        fontWeight: 'bold',
+        lineHeight: '120%',
       },
     },
   },
