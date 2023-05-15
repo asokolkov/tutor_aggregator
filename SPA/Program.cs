7812dfbebc635ec18fa5879636cf4afa366e882a
Ilya Zhdanov <zdravulin@gmail.com>
2023-05-15 21:16:39 +0500

    попытка 100500

--- SPA/Program.cs
+++ SPA/Program.cs
@@ -156,6 +156,6 @@ app.UseAuthorization();
 
 app.UseEndpoints(configure => { configure.MapControllers(); });
 
-app.MapFallbackToFile("wwwroot/index.html");
+app.MapFallbackToFile("index.html");
 
 app.Run();
