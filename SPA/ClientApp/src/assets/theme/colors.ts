fb48cd294ecd48ae829f01b99e821e8bb2140de5
Artemii Kurganov <aokurganov@mail.ru>
2023-05-16 21:42:02 +0500

    Feat: theme-enum

--- SPA/ClientApp/src/assets/theme/colors.ts
+++ SPA/ClientApp/src/assets/theme/colors.ts
@@ -1,12 +1,9 @@
 export const colors = {
-  blue: {
+  'custom.blue': {
     100: '#C4D7E2',
     200: '#7C98AA',
     300: '#2A363D',
   },
-  green: '#59952A',
-  red: '#A91A1A',
-  gray: '#E2E8F0',
-  white: '#FFFFFF',
-  black: '#000000',
+  'custom.green': '#59952A',
+  'custom.red': '#A91A1A',
 };
