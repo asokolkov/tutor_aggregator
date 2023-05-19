ef3d0093fbf34afb548a74978d10a054f76240af
Aleksey <a.e.sokolkov@gmail.com>
2023-05-20 01:10:21 +0500

    fix: removed useless properties from page and page dto

--- SPA/Domain/Page.cs
+++ SPA/Domain/Page.cs
@@ -5,25 +5,17 @@ public sealed class Page<T>
     public ICollection<T> Items { get; }
 
     public long TotalCount { get; }
-    
-    public int LastPage { get; }
 
-    public int CurrentPage { get; }
-    
-    public int Size { get; }
-    
     public bool HasPrevious { get; }
     
     public bool HasNext { get; }
 
     public Page(ICollection<T> items, long totalCount, int currentPage, int size)
     {
+        var lastPage = (int)Math.Ceiling(totalCount / (double)size) - 1;
         Items = items;
         TotalCount = totalCount;
-        Size = size;
-        CurrentPage = currentPage;
-        LastPage = (int)Math.Ceiling(totalCount / (double)size) - 1;
-        HasPrevious = 0 < currentPage && currentPage <= LastPage + 1;
-        HasNext = currentPage < LastPage;
+        HasPrevious = 0 < currentPage && currentPage <= lastPage + 1;
+        HasNext = currentPage < lastPage;
     }
 }
--- SPA/V1/DataModels/V1PageDto.cs
+++ SPA/V1/DataModels/V1PageDto.cs
@@ -9,16 +9,7 @@ public sealed class V1PageDto<T>
 
     [JsonProperty("totalCount")]
     public long TotalCount { get; init; }
-    
-    [JsonProperty("lastPage")] 
-    public int LastPage { get; init; }
 
-    [JsonProperty("currentPage")] 
-    public int CurrentPage { get; init; }
-    
-    [JsonProperty("size")] 
-    public int Size { get; init; }
-    
     [JsonProperty("hasPrevious")]
     public bool HasPrevious { get; init; }
     
