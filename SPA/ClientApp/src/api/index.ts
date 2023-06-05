4f6baaa1cba48337eb34288c83894cb3f8f39d9a
Artemii Kurganov <aokurganov@mail.ru>
2023-06-05 23:03:07 +0500

    Feat: Работа с аватарками

--- SPA/ClientApp/src/api/index.ts
+++ SPA/ClientApp/src/api/index.ts
@@ -4,12 +4,15 @@ import SubjectsAPI from './subjects';
 import StudentAPI from './students';
 import TutorsAPI from './tutors';
 import UserAPI from './user';
+import AvatarAPI from './avatars';
 
-export const apiControllers = {
+export const api = {
   LessonsAPI,
   LocationAPI,
   StudentAPI,
   SubjectsAPI,
   TutorsAPI,
   UserAPI,
+  AvatarAPI,
 };
+
