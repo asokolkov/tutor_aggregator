fbcb9d7cc96728b2cf765ed9cbe8d8729b0f6777
Artemii Kurganov <aokurganov@mail.ru>
2023-06-06 20:54:29 +0500

    Fix: Error messages

--- SPA/ClientApp/src/components/Errors/ErrorPage.tsx
+++ SPA/ClientApp/src/components/Errors/ErrorPage.tsx
@@ -1,31 +1,40 @@
 import * as React from 'react';
-import { HStack, Text, Image, VStack, Flex } from '@chakra-ui/react';
+import { HStack, Text, Image, VStack, Flex, Button } from '@chakra-ui/react';
 import teacherIcon from '../../assets/images/teacher_icon.png';
-import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom';
 import { AxiosError } from 'axios';
+import { MAIN_PAGE } from '../../routes/routePaths';
 
 export const ErrorPage: React.FC = () => {
   const error = useRouteError();
-  const isRouteResponse = isRouteErrorResponse(error);
+  const isNotFound = isRouteErrorResponse(error) && error.status === 404;
   const isNetworkError =
     error instanceof AxiosError && error.message === 'Network Error';
+
+  let errorMessage: string;
+  if (isNotFound) errorMessage = 'Страница не найдена';
+  else if (isNetworkError) errorMessage = 'Проверьте соединение с интернетом';
+  else errorMessage = 'Пожалуйста, повторите попытку позже';
 
   return (
     <Flex align="center" justify="center" minH="100vh">
       <HStack spacing="64px">
         {!isNetworkError && <Image src={teacherIcon} w="312px" />}
-        <VStack>
-          <Text variant="regular.h1">Произошла ошибка!</Text>
-          <Text variant="regular.h2">
-            {isRouteResponse && error.status === 404
-              ? 'Страница не найдена'
-              : 'Пожалуйста, повторите попытку позже'}
-          </Text>
-          <Text>
-            {!isRouteResponse &&
-              error instanceof AxiosError &&
-              'Произошла ошибка при запросе. Пожалуйста, повторите поптыку позже.'}
-          </Text>
+        <VStack spacing="32px">
+          <VStack>
+            <Text variant="regular.h1">Произошла ошибка!</Text>
+            <Text variant="regular.h2">{errorMessage}</Text>
+          </VStack>
+
+          {isNotFound ? (
+            <Link to={MAIN_PAGE}>
+              <Button>Вернуться на главную</Button>
+            </Link>
+          ) : (
+            <Button onClick={() => window.location.reload()}>
+              Обновить страницу
+            </Button>
+          )}
         </VStack>
       </HStack>
     </Flex>
