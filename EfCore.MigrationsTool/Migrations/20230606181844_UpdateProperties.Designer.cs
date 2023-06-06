5cd9014932c9d7bc1240daf6b543e61ef123975b
Ilya Zhdanov <zdravulin@gmail.com>
2023-06-06 23:24:52 +0500

    feat: обновил миграцию

--- EfCore.MigrationsTool/Migrations/20230606181844_UpdateProperties.Designer.cs
+++ EfCore.MigrationsTool/Migrations/20230606181844_UpdateProperties.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EFCore.Postgres.Application.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EfCore.MigrationsTool.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230606181844_UpdateProperties")]
+    partial class UpdateProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
