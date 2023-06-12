5b0af2712913087039290bee61fff66579da6dcf
Artemii Kurganov <aokurganov@mail.ru>
2023-06-12 14:51:33 +0500

    PPU_FIIT_AR-122 После обновления аватара пользователя новый аватар показывается только после перезагрузки страницы

--- SPA/ClientApp/src/pages/Profiles/components/AvatarSection.tsx
+++ SPA/ClientApp/src/pages/Profiles/components/AvatarSection.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
 import { Avatar, Button, Flex, useBreakpointValue } from '@chakra-ui/react';
 import { FileUpload } from './FileUpload';
-import { ChangeEvent, useContext } from 'react';
+import { ChangeEvent, useContext, useState } from 'react';
 import AvatarAPI from '../../../api/avatars';
 import { UserContext } from '../../../layouts/base/contexts/UserContext';
 import { getFullName } from '../../../utils/names';
 import { getAvatarUri } from '../../../utils/helper';
 
 export const AvatarSection: React.FC = () => {
+  const { user } = useContext(UserContext);
+  const [avatarSrc, setAvatarSrc] = useState<string>(getAvatarUri(user.id));
+
   const onAvatarChange = async (event: ChangeEvent<HTMLInputElement>) => {
     const avatarFile = event.target.files[0];
     await AvatarAPI.uploadAvatar(avatarFile);
+    setAvatarSrc(getAvatarUri(`${user.id}?${new Date().getTime()}`));
   };
-  const { user } = useContext(UserContext);
 
   const isDesktop = useBreakpointValue({ base: false, lg: true });
 
@@ -22,7 +25,7 @@ export const AvatarSection: React.FC = () => {
         w={'10em'}
         h={'10em'}
         name={getFullName(user.firstName, user.lastName)}
-        src={getAvatarUri(user.id)}
+        src={avatarSrc}
         margin={'0 0 10px 0'}
         colorScheme={'blue'}
         showBorder
