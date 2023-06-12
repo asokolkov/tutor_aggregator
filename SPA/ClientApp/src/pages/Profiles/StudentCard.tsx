f91fafa9fe08afca581a127d7d66e4132d8e92a7
Artemii Kurganov <aokurganov@mail.ru>
2023-06-12 14:37:33 +0500

    PPU_FIIT_AR-119 Добавить поле "О себе" в профиле репетитора

--- SPA/ClientApp/src/pages/Profiles/StudentCard.tsx
+++ SPA/ClientApp/src/pages/Profiles/StudentCard.tsx
@@ -1,27 +1,19 @@
 import * as React from 'react';
 import { useContext } from 'react';
 import { Box, Flex, useBreakpointValue } from '@chakra-ui/react';
-import { SubmitButton } from './components/SubmitButton';
 import profileIcon from '../../assets/images/profile_icon_bg.png';
 import { InputRow } from './components/InputRow';
 import { ProfileContext } from './contexts/ProfileContext';
 import { Form, Formik } from 'formik';
-import StudentAPI from '../../api/students';
 import { TooltipType } from './components/_shared';
 import { SelectOptionsRow } from './components/SelectOptionsRow';
-import { TextAreaRow } from './components/TextAreaRow';
 import { AvatarSection } from './components/AvatarSection';
-import { StudentInitValues, useStudentForm } from './hooks/useForm';
+import { useStudentForm } from './hooks/useForm';
 
 export const StudentCard: React.FC = () => {
   const isDesktop = useBreakpointValue({ base: false, lg: true });
   const { student } = useContext(ProfileContext);
-  const { updateStudent, mapStudent } = useStudentForm(student);
-
-  const onSubmit = async (values: StudentInitValues) => {
-    const newStudent = updateStudent(values);
-    await StudentAPI.putCurrentProfileValues(newStudent);
-  };
+  const { mapStudent } = useStudentForm(student);
 
   return (
     <Box
@@ -35,7 +27,7 @@ export const StudentCard: React.FC = () => {
       backgroundRepeat={'no-repeat'}
       backgroundSize={'14em'}
     >
-      <Formik initialValues={mapStudent()} onSubmit={onSubmit}>
+      <Formik initialValues={mapStudent()} onSubmit={() => {}}>
         <Form>
           <Flex
             padding={isDesktop ? '1.5em 5em 1.5em 3em' : '1em 1em 1em 1em'}
@@ -70,17 +62,6 @@ export const StudentCard: React.FC = () => {
                   type: TooltipType.Lock,
                 }}
               />
-              <TextAreaRow
-                label={'О себе'}
-                placeholder={'Увлекаюсь горными лыжами и версткой сайтов'}
-                name={'about'}
-                tooltip={{
-                  label:
-                    'Напишите небольшое описание, которое будет отображаться на вашей карточке',
-                  type: TooltipType.Info,
-                }}
-              />
-              <SubmitButton buttonText={'Сохранить'} />
             </Flex>
           </Flex>
         </Form>
